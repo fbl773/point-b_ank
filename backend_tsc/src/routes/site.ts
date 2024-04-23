845368e796eb660e01ecd963e7d92cafd8da68ff
Bennett Lewis <bennettwlewis@hotmail.com>
2024-04-22 19:25:23 -0600

    yea we need the files

---
+++ backend_tsc/src/routes/site.ts
@@ -0,0 +1,15 @@
+import {NextFunction, Request, Response, Router} from "express";
+import Site, {ISite} from "../entitites/site";
+import authenticate from "../utilities/jwt_utils";
+import crud_factory from "../utilities/crud_factory";
+
+const site_router= Router();
+
+/* Give them the CRUD treatment*/
+crud_factory.create<ISite>(Site,site_router,authenticate,"site") //Handles POST /sites
+crud_factory.read_all<ISite>(Site,site_router,authenticate,"site") // handles GET /sites
+crud_factory.read_one<ISite>(Site,site_router,authenticate,"site") // handles GET /site/:id
+crud_factory.update_one<ISite>(Site,site_router,authenticate,"site") // handles PUT /site/:id
+crud_factory.delete_one<ISite>(Site,site_router,authenticate,"site") // handles DELETE /site/:id
+
+export default site_router;
---
+++ backend_tsc/src/utilities/crud_factory.ts
@@ -0,0 +1,103 @@
+import {Model} from "mongoose"
+import {NextFunction, Request, Response, Router} from "express";
+
+function create<T>(model:Model<T>,
+                         router:Router,
+                         authenticate:(req:Request,res:Response,next:NextFunction)=>Promise<any>,
+                         entity_name?:string){
+    //Give the router the create request for the entity
+    router.post("/",
+        authenticate,
+        (req:Request,res:Response,_next:NextFunction) => {console.log("TODO:Validation RULES"); _next();}, //This seems silly actually their use could be handled on client side
+        (_req:Request,_res:Response,_next:Function) => {console.log("TODO: VALIDATE"); _next()},
+        (req: Request, res: Response) => {
+            let new_entity: T = req.body;
+            model.create(new_entity)
+                .then((nc) => res.status(201).send(
+                    {
+                        message:`Created new ${entity_name}`,
+                        id:nc._id
+                    }))
+                .catch(err => res.status(404).send({message:`Failed to create ${entity_name}`,err}))
+        }
+    );
+}
+
+function read_all<T>(model:Model<T>,
+                           router:Router,
+                           authenticate:(req:Request,res:Response,next:NextFunction)=>Promise<any>,
+                           entity_name?:string){
+    //Give the router the create request for the entity
+    router.get("/",
+        authenticate,
+        (req:Request,res:Response,_next:NextFunction) => {console.log("TODO:Validation RULES"); _next();}, //This seems silly actually their use could be handled on client side
+        (_req:Request,_res:Response,_next:Function) => {console.log("TODO: VALIDATE"); _next()},
+        (req: Request, res: Response) => {
+            model.find({})
+                .then((entities) => res.status(201).send(entities))
+                .catch(err => res.status(404).send({message:`Failed to find${entity_name}s`,err}))
+        }
+    );
+}
+function read_one<T>(model:Model<T>,
+                           router:Router,
+                           authenticate:(req:Request,res:Response,next:NextFunction)=>Promise<any>,
+                           entity_name:string = "Unspecified Entity"){
+    //Give the router the create request for the entity
+    router.get("/:id",
+        authenticate,
+        (req:Request,res:Response,_next:NextFunction) => {console.log("TODO:Validation RULES"); _next();}, //This seems silly actually their use could be handled on client side
+        (_req:Request,_res:Response,_next:Function) => {console.log("TODO: VALIDATE"); _next()},
+        (req: Request, res: Response) => {
+            model.findById(req.params.id ?? "NO ID")
+                .then(cat => res.status(200).send(cat))
+                .catch(err => res.status(404).send({message:`Failed to find ${entity_name} ${req.params.id}`,err}))
+        }
+    );
+}
+
+function update_one<T>(model:Model<T>,
+                             router:Router,
+                             authenticate:(req:Request,res:Response,next:NextFunction)=>Promise<any>,
+                             entity_name?:string){
+    //Give the router the create request for the entity
+    router.put("/:id",
+        authenticate,
+        (req:Request,res:Response,_next:NextFunction) => {console.log("TODO:Validation RULES"); _next();}, //This seems silly actually their use could be handled on client side
+        (_req:Request,_res:Response,_next:Function) => {console.log("TODO: VALIDATE"); _next()},
+        (req: Request, res: Response) => {
+            let new_ent = req.body;
+            model.findOneAndUpdate({_id:req.params.id},new_ent,{new:true})
+                .then((updated) => res.status(200).send(
+                    {
+                        message:`Successfully updated ${req.params.id}`,
+                        updated
+                    }))
+                .catch((err) => res.status(406).send({message:`failed to update ${entity_name} ${req.params.id}`,err}))
+        }
+    );
+}
+function delete_one<T>(model:Model<T>,
+                             router:Router,
+                             authenticate:(req:Request,res:Response,next:NextFunction)=>Promise<any>,
+                             entity_name?:string){
+    //Give the router the create request for the entity
+    router.delete("/:id",
+        authenticate,
+        (req:Request,res:Response,_next:NextFunction) => {console.log("TODO:Validation RULES"); _next();}, //This seems silly actually their use could be handled on client side
+        (_req:Request,_res:Response,_next:Function) => {console.log("TODO: VALIDATE"); _next()},
+        (req: Request, res: Response) => {
+            model.findOneAndDelete({_id:req.params.id},{})
+                .then((dc) => {
+                    if(dc)
+                        res.status(200).send({message:`Successfully deleted ${entity_name} ${req.params.id}`})
+                    else
+                        res.status(404).send({message:`No such ${entity_name}`})
+        })
+                .catch((err) => {res.status(500).send({message:`failed to delete ${entity_name} ${req.params.id}`,err})})
+        }
+    );
+}
+
+
+export default {create,read_all,read_one,update_one,delete_one}
