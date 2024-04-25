e6140706979029d6774a298f6fe88a18b2025e77
Bennett Lewis <bennettwlewis@hotmail.com>
2024-04-25 14:39:52 -0600

    added culture, need to test though

--- backend_tsc/src/entitites/projectile_point.ts
+++ backend_tsc/src/entitites/projectile_point.ts
@@ -1,5 +1,8 @@
 import {Schema,Types,Model,model} from "mongoose"
 import {IArtifact} from "./mongo_entity";
+import Material from "./material";
+import Culture from "./culture";
+import Site from "./site";
 
 export interface IProjectilePoint extends IArtifact{
     name: string,
@@ -15,9 +18,9 @@ const projectile_pointSchema = new Schema<IProjectilePoint,ProjectilePointModal>
     name:{type:String, required:true},
     image:{type:String, required:false},
     description:{type:String, required:false},
-    culture:{type:Types.ObjectId,required:false},
-    material:{type:Types.ObjectId,ref:"Material",required:false},
-    site_id:{type:Types.ObjectId,ref:"Site",required:true},
+    culture:{type:Types.ObjectId,ref:Culture,required:false},
+    material:{type:Types.ObjectId,ref:Material,required:false},
+    site_id:{type:Types.ObjectId,ref:Site,required:true},
     //These could be set by "culture" templates OR hard-coded... maybe a good use of sub document here?
     blade_shape:{type:String,required:false,enum:["TODO","TODO1"]},
     base_shape:{type:String,required:false,enum:["TODO","TODO1"]},//TODO: Design decision about indeterminate handling. Backend or front?
