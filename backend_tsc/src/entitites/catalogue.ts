dade2dc978067336840d38eeea05999aec0c6b39
Bennett Lewis <bennettwlewis@hotmail.com>
2024-04-26 15:11:57 -0600

    getting close

--- backend_tsc/src/entitites/catalogue.ts
+++ backend_tsc/src/entitites/catalogue.ts
@@ -1,6 +1,7 @@
 import {Schema,model,Model} from "mongoose"
 import {IMongo_Entity} from "./mongo_entity";
-import Site from "./site";
+import Site, {ISite} from "./site";
+import {cascade_related} from "../utilities/trigger_factory";
 
 export interface ICatalogue extends IMongo_Entity{
    name: string,
@@ -19,10 +20,21 @@ const catalogueSchema = new Schema<ICatalogue,CatalogueModel>({
  * Cascade delete related sites
  */
 catalogueSchema.pre("findOneAndDelete", async function(next){
-    let self = await this.model.findOne(this.getFilter());
-    await Site.deleteMany({catalogue_id:self._id});
+    await cascade_related<ICatalogue,ISite>(this.model,this.getFilter(),Site,"catalogue_id");
     next();
-})
+});
+
+catalogueSchema.pre("deleteOne", async function(next){
+    let self = await this.model.findOne(this.getFilter());
+    await cascade_related<ICatalogue,ISite>(this.model,this.getFilter(),Site,"catalogue_id");
+    next();
+});
+
+// Idea calling
+//cascade_delete<CascadeMe>("triggeringMethod",schema)
+//cascade_update<CascadeMe>("triggeringMethod",update)
+
+
 const Catalogue:CatalogueModel = model<ICatalogue,CatalogueModel>('Catalogue',catalogueSchema);
 
 export default Catalogue;
