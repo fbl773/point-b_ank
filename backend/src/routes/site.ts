2ec257052c5bc1438b8ae9c0034b778f228d7e2a
Bennett Lewis <bennettwlewis@hotmail.com>
2024-11-26 14:45:42 -0600

    we can send a file back. and it is super janky and not general in the slightest. but we will overcome!

--- backend/src/routes/site.ts
+++ backend/src/routes/site.ts
@@ -39,6 +39,7 @@ crud_factory.find_related<IProjectilePoint>("/:id/points",ProjectilePoint,site_r
     "site_id",authenticate,"site");
 
 
-file_utils.upload_one(site_router,authenticate,site_multer.create_single(),"/:site_id/upload/:point_id","site/point");
+file_utils.upload_one(site_router,authenticate,site_multer.create_single(),"/:site_id/upload/:point_id","site-point");
+file_utils.get_img(site_router,authenticate,"/:site_id/:point_id","sites","site_id","point_id")
 
 export default site_router;
--- backend/src/utilities/file_utils.ts
+++ backend/src/utilities/file_utils.ts
@@ -1,4 +1,5 @@
 import {Request, Response, NextFunction, Router, RequestHandler} from "express";
+import path from "path";
 
 function upload_one(router: Router,
                     authenticate: (req: Request, res: Response, next: NextFunction) => Promise<any>,
@@ -15,4 +16,19 @@ function upload_one(router: Router,
         })
 }
 
-export default{upload_one}
+function get_img(router: Router,
+                      authenticate: (req: Request, res: Response, next: NextFunction) => Promise<any>,
+                      endpoint: string,
+                      host_dir: string,
+                      primary_id:string,
+                      secondary_id:string){
+    router.get(endpoint,
+        authenticate,
+        (req:Request, res:Response)=>{
+        let file_name = `uploads/${host_dir}/${req.params[primary_id]}/${req.params[secondary_id]}` + ".JPG";
+        console.log(`TRYING TO GET ${file_name}`);
+        res.sendFile(path.join("/Users/FrankyB/fGrams/Projects/point-b_ank/backend",file_name));
+    })
+}
+
+export default{upload_one,get_img}
